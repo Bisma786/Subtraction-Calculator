e04bdf7b19ac702dee1475b47d31792ee7430f76
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-03-05 00:24:07 +0500

    Add files via upload

---
+++ subcalc.ts
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const input1=await inquirer.prompt({
+    name:"val1",
+    type:"number",
+    message:"Enter your fist number",
+})
+
+const input2=await inquirer.prompt({
+    name:"val2",
+    type:"number",
+    message:"Enter your 2nd value"
+})
+let total= input1.val1-input2.val2;
+console.log(total)
